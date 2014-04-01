369fe3ba033e332c60c5d86ed7b87acbc5f0a388
He Liu <liulonnie@gmail.com>
2014-03-31 18:37:20 -0700

    split the entry functions

--- lab1.go
+++ lab1.go
@@ -9,11 +9,6 @@ func NewClient(addr string) trib.Storage {
 	panic("todo")
 }
 
-// Makes a front end that talks to a list of backends
-func NewFront(backs []string) trib.Server {
-	panic("todo")
-}
-
 // Serve as a backend based on the given configuration
 func ServeBack(b *trib.BackConfig) error {
 	panic("todo")
