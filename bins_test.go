322a68259e782fe3127c0f91b2b4aea90a6e41f7
He Liu <liulonnie@gmail.com>
2014-04-20 00:10:09 -0700

    starting keeper in bins_test

--- bins_test.go
+++ bins_test.go
@@ -15,6 +15,9 @@ import (
 func TestBinStorage(t *testing.T) {
 	addr1 := randaddr.Local()
 	addr2 := randaddr.Local()
+	for addr2 == addr1 {
+		addr2 = randaddr.Local()
+	}
 
 	ready1 := make(chan bool)
 	ready2 := make(chan bool)
@@ -33,6 +36,28 @@ func TestBinStorage(t *testing.T) {
 	r := <-ready1 && <-ready2
 	if !r {
 		t.Fatal("not ready")
+	}
+
+	readyk := make(chan bool)
+	addrk := randaddr.Local()
+	for addrk == addr1 || addrk == addr2 {
+		addrk = randaddr.Local()
+	}
+	go func() {
+		e := triblab.ServeKeeper(&trib.KeeperConfig{
+			Backs: []string{addr1, addr2},
+			Addrs: []string{addrk},
+			This:  0,
+			Id:    0,
+			Ready: readyk,
+		})
+		if e != nil {
+			t.Fatal(e)
+		}
+	}()
+
+	if !<-readyk {
+		t.Fatal("keeper not ready")
 	}
 
 	bc := triblab.NewBinClient(
