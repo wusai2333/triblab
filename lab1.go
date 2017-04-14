aa3646aeaf6abeb9b46e91e3191b636c02008b9c
Sai Wu <cse223_saw044@vm162.sysnet.ucsd.edu>
2017-04-14 00:08:13 -0700

    update

--- lab1.go
+++ lab1.go
@@ -2,14 +2,45 @@ package triblab
 
 import (
 	"trib"
+	"fmt"
+	"net"
+	"net/rpc"
+	"net/http"
 )
 
 // Creates an RPC client that connects to addr.
 func NewClient(addr string) trib.Storage {
-	panic("todo")
+	return &client{Addr:addr}
 }
 
 // Serve as a backend based on the given configuration
 func ServeBack(b *trib.BackConfig) error {
-	panic("todo")
+	server := rpc.NewServer()
+	rpc.HandleHTTP()
+	e := server.RegisterName("Storage", b.Store)
+
+	if e != nil {
+		fmt.Println(e)
+		if b.Ready != nil {
+			b.Ready <- false
+		}
+		return e
+	}
+	
+	l, e := net.Listen("tcp", b.Addr)
+	
+	if e != nil {
+		fmt.Println(e)
+		if b.Ready != nil {
+			b.Ready <- false
+		}
+		return e
+	}
+	
+	if b.Ready != nil {
+		b.Ready <- true
+	}
+	
+	return http.Serve(l, nil)
 }
+
