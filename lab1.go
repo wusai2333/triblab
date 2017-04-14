3333c57a042a0db5cb56fe8e04319e22661e680f
wusai2333 <wusai2333@hotmail.com>
2017-04-14 03:41:43 -0700

    func (self *client) Get(key string, value *string) error { 	conn, e := rpc.DialHTTP("tcp", self.Addr) 	if e != nil { 		return e 	}
    	// perform the call
    	e = conn.Call("Storage.Get", key, value)
    	if e != nil {
    		conn.Close()
    		return e
    	}
    
    	log.Println(self, ".Get:", "key:", key, "value:", *value)
    
    	// close the connection
    	return conn.Close()
    }
    
    func (self *client) Clock(atLeast uint64, ret *uint64) error {
    	// connect to the server
    	conn, e := rpc.DialHTTP("tcp", self.Addr)
    	if e != nil {
    		return e
    	}
    
    	// perform the call
    	e = conn.Call("Storage.Clock", atLeast, ret)
    	if e != nil {
    		conn.Close()
    		return e
    	}
    
    	log.Println(self, ".Clock:", "atLeast:", atLeast, "ret:", *ret)
    
    	// close the connection
    	return conn.Close()
    }
    
    func (self *client) Set(kv *trib.KeyValue, succ *bool) error {
    	// connect to the server
    	conn, e := rpc.DialHTTP("tcp", self.Addr)
    	if e != nil {
    		return e
    	}
    
    	// perform the call
    	e = conn.Call("Storage.Set", kv, succ)
    	if e != nil {
    		conn.Close()
    		return e
    	}
    
    	log.Println(self, ".Set:", "kv:", kv, "succ:", *succ)
    	// close the connection
    	return conn.Close()
    }
    
    func (self *client) Keys(p *trib.Pattern, r *trib.List) error {
    	// connect to the server
    	conn, e := rpc.DialHTTP("tcp", self.Addr)
    	if e != nil {
    		return e
    	}
    
    	r.L = nil
    
    	// perform the call
    	e = conn.Call("Storage.Keys", p, r)
    	if e != nil {
    		conn.Close()
    		return e
    	}
    
    	if r.L == nil {
    		r.L = []string{}
    	}
    
    	log.Println(self, ".Keys:", "p:", *p, "r:", *r)
    
    	// close the connection
    	return conn.Close()
    }
    
    func (self *client) ListKeys(p *trib.Pattern, r *trib.List) error {
    	// connect to the server
    	conn, e := rpc.DialHTTP("tcp", self.Addr)
    	if e != nil {
    		return e
    	}
    
    	r.L = nil
    
    	// perform the call
    	e = conn.Call("Storage.ListKeys", p, r)
    	if e != nil {
    		conn.Close()
    		return e
    	}
    
    	if r.L == nil {
    		r.L = []string{}
    	}
    
    	log.Println(self, ".ListKeys:", "p:", *p, "r:", *r)
    
    	// close the connection
    	return conn.Close()
    }
    
    func (self *client) ListGet(key string, ret *trib.List) error {
    	// connect to the server
    	conn, e := rpc.DialHTTP("tcp", self.Addr)
    	if e != nil {
    		return e
    	}
    
    	ret.L = nil
    
    	// perform the call
    	e = conn.Call("Storage.ListGet", key, ret)
    	if e != nil {
    		conn.Close()
    		return e
    	}
    
    	if ret.L == nil {
    		ret.L = []string{}
    	}
    
    	log.Println(self, ".ListGet:", "key:", key, "ret:", *ret)
    
    	// close the connection
    	return conn.Close()
    }
    
    func (self *client) ListAppend(kv *trib.KeyValue, succ *bool) error {
    	// connect to the server
    	conn, e := rpc.DialHTTP("tcp", self.Addr)
    	if e != nil {
    		return e
    	}
    
    	// perform the call
    	e = conn.Call("Storage.ListAppend", kv, succ)
    	if e != nil {
    		conn.Close()
    		return e
    	}
    
    	log.Println(self, ".ListAppend:", "kv:", kv.Key, kv.Value, "succ:", *succ)
    
    	// close the connection
    	return conn.Close()
    }
    
    func (self *client) ListRemove(kv *trib.KeyValue, n *int) error {
    	// connect to the server
    	conn, e := rpc.DialHTTP("tcp", self.Addr)
    	if e != nil {
    		return e
    	}
    
    	// perform the call
    	e = conn.Call("Storage.ListRemove", kv, n)
    	if e != nil {
    		conn.Close()
    		return e
    	}
    
    	log.Println(self, ".ListRemove:", "kv:", kv, "n:", *n)
    
    	// close the connection
    	return conn.Close()
    }

--- lab1.go
+++ lab1.go
@@ -15,7 +15,7 @@ func NewClient(addr string) trib.Storage {
 
 // Serve as a backend based on the given configuration
 func ServeBack(b *trib.BackConfig) error {
-	server := new(server)
+	server := NewServer()
 	e := server.RegisterName("Storage", b.Store)
 	rpc.HandleHTTP()
 
